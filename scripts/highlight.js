87e444571cd68bf10986ca4799b2b4f89484d7bb
Alessandro Dias <alessandro.dias@novahaus.com.br>
2018-08-31 15:26:52 -0300

    Highlight hovered element

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,92 +1,38 @@
 (function() {
 
-	var elements = null;
-	var tracks = null;
-	var thumbs = null;
-	var incrementButtons = null;
-	var decrementButtons = null;
-	var corner = null;
+	var triggers = null;
+	var holder = null;
 
 	function construct() {
-		elements = document.querySelector('[data-elements]');
-		tracks = document.querySelector('[data-tracks]');
-		thumbs = document.querySelector('[data-thumbs]');
-		incrementButtons = document.querySelector('[data-increment-buttons]');
-		decrementButtons = document.querySelector('[data-decrement-buttons]');
-		corner = document.querySelector('[data-corner]');
+		triggers = document.querySelectorAll('[data-highlight]');
+		holder = document.querySelector('[data-holder]');
 
-		reportElements();
+
 		setupListeners();
 	}
 
 	function setupListeners() {
-		document.querySelector('[data-highlight-elements]').addEventListener('mouseover', function() {
-			onMouseOver(elements);
-		})
-		document.querySelector('[data-highlight-elements]').addEventListener('mouseleave', function() {
-			onMouseLeave(elements);
-		});
+		for (var i = 0; i < triggers.length; i++) {
+			triggers[i].addEventListener('mouseover', function(event) {
+				onMouseOver(event, holder);
+			});
+		}
 
-		document.querySelector('[data-highlight-tracks]').addEventListener('mouseover', function() {
-			onMouseOver(tracks);
-		})
-		document.querySelector('[data-highlight-tracks]').addEventListener('mouseleave', function() {
-			onMouseLeave(tracks);
-		});
-
-		document.querySelector('[data-highlight-thumbs]').addEventListener('mouseover', function() {
-			onMouseOver(thumbs);
-		})
-		document.querySelector('[data-highlight-thumbs]').addEventListener('mouseleave', function() {
-			onMouseLeave(thumbs);
-		});
-
-		document.querySelector('[data-highlight-increment-buttons]').addEventListener('mouseover', function() {
-			onMouseOver(incrementButtons);
-		})
-		document.querySelector('[data-highlight-increment-buttons]').addEventListener('mouseleave', function() {
-			onMouseLeave(incrementButtons);
-		});
-
-		document.querySelector('[data-highlight-decrement-buttons]').addEventListener('mouseover', function() {
-			onMouseOver(decrementButtons);
-		})
-		document.querySelector('[data-highlight-decrement-buttons]').addEventListener('mouseleave', function() {
-			onMouseLeave(decrementButtons);
-		});
-
-		document.querySelector('[data-highlight-corner]').addEventListener('mouseover', function() {
-			onMouseOver(corner);
-		})
-		document.querySelector('[data-highlight-corner]').addEventListener('mouseleave', function() {
-			onMouseLeave(corner);
-		});
-	}
-
-	function onMouseOver(target) {
-		target.classList.add('is-highlighted');
-
-		var spans = target.querySelectorAll('span');
-		for (var i = 0; i < spans.length; i++) {
-			spans[i].classList.add('is-highlighted');
+		for (var i = 0; i < triggers.length; i++) {
+			triggers[i].addEventListener('mouseleave', function(event) {
+				onMouseLeave(event, holder);
+			});
 		}
 	}
 
-	function onMouseLeave(target) {
-		target.classList.remove('is-highlighted');
-
-		var spans = target.querySelectorAll('span');
-		for (var i = 0; i < spans.length; i++) {
-			spans[i].classList.remove('is-highlighted');
-		}
+	function onMouseOver(event, target) {
+		var partToHighlight = event.currentTarget.getAttribute('data-highlight');
+		target.classList.add('highlight-' + partToHighlight);
 	}
 
-	function reportElements() {
-		console.log('elements', elements);
-		console.log('tracks', tracks);
-		console.log('thumbs', thumbs);
-		console.log('incrementButtons', incrementButtons);
-		console.log('decrementButtons', decrementButtons);
+	function onMouseLeave(event, target) {
+		var partToFade = event.currentTarget.getAttribute('data-highlight');
+		target.classList.remove('highlight-' + partToFade);
 	}
 
 	construct();
